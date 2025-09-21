99fbc1f80cacf1f72f30d6c3e52a9d3a1cb576cd
Houssam HAMOUDA <z23hhous@gmail.com>
2025-09-21 16:13:30 +0100

    Update mailmultiply.sh

--- mailmultiply.sh
+++ mailmultiply.sh
@@ -16,7 +16,7 @@ ${White}| |  | | (_| | | | ${Red}| |  | | |_| | | |_| | |_) | | |_| |
 ${White}|_|  |_|\__,_|_|_| |_|  |_|\__,_|_|\__|_| .__/|_|\__, |
 ${White}                                        | |       __/ |
 ${White}     Hola! Create Unlimited Gmails      |_|      |___/ \n
-         \e[0;96mDeveloped by: ${Red}Sandesh (3xploitGuy)\n\n"
+         \e[0;96mDeveloped by: ${Red}hOuSs@m (s43iD)\n\n"
 }
 
 email () {
